1c1e992daf489210a4bda086d7f3253c5c18c7af
leydet4 <109769769+leydet4@users.noreply.github.com>
2026-02-19 14:20:44 -0500

    asd

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,35 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+const CACHE_NAME = "pov-sar-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/gar.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
-self.addEventListener("fetch", () => {});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
