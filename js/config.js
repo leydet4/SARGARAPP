51c2092b80b9f7052f7f1cdf4cbe4313f25d0d87
leydet4 <109769769+leydet4@users.noreply.github.com>
2026-02-19 07:59:20 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const config = {
+  stations: {
+    primary: "8638863",
+    secondary: "8638610"
+  },
+  buoyFallback: "44014",
+  forms: {
+    tf1: "#",
+    tf2: "#",
+    tf3: "#",
+    tf4: "#",
+    admin: "#"
+  }
+};
