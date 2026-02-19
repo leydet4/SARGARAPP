744d27dd25d55e5d568de755dd43cf6008bb37c4
leydet4 <109769769+leydet4@users.noreply.github.com>
2026-02-19 11:27:33 -0500

    service

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,8 @@
-const CACHE_NAME = "pov-sar-cache-v1";
-const urlsToCache = [
-"/",
-"/index.html",
-"/gar.html",
-"/manifest.json"
-];
-
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+// Only cache site assets, NOT external APIs
+if (event.request.url.includes(".netlify/functions")) return;
 });
