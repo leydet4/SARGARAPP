b268670322477992b128292b004f9e71d38b887d
leydet4 <109769769+leydet4@users.noreply.github.com>
2026-02-20 20:00:09 -0500

    Update getResources.js

--- netlify/functions/getResources.js
+++ netlify/functions/getResources.js
@@ -1,35 +1,6 @@
-const { getStore } = require("@netlify/blobs");
-
 exports.handler = async function () {
-  try {
-    const metaStore = getStore("resources-meta");
-
-    const raw = await metaStore.get("list.json");
-
-    if (!raw) {
-      await metaStore.set("list.json", JSON.stringify([]));
-      return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify([])
-      };
-    }
-
-    let list = JSON.parse(raw);
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-store"
-      },
-      body: JSON.stringify(list)
-    };
-
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify([{ test: "working" }])
+  };
 };
