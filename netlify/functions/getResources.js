d4d4b5fad5c2238aabb73fc39d3fb9dec5b1ffee
leydet4 <109769769+leydet4@users.noreply.github.com>
2026-02-20 19:09:58 -0500

    Update getResources.js

--- netlify/functions/getResources.js
+++ netlify/functions/getResources.js
@@ -9,7 +9,12 @@ export default async () => {
     JSON.stringify(list || []),
     {
       status: 200,
-      headers: { "Content-Type": "application/json" }
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+        "Pragma": "no-cache",
+        "Expires": "0"
+      }
     }
   );
 };
